40160ce08568b812e9ec813b7449c0f8c4b2f5db
leandro alfonso <leamanya24.6@gmail.com>
2020-09-15 19:56:54 -0300

    Añadido espadachin y escudo

---
+++ src/Library/Escudo.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Library
+{
+
+
+    public class Escudo
+    {
+
+        public Escudo(int defensa)
+        {
+            this.Defensa = defensa;
+        }
+        public int Defensa {get;set;}
+    }
+}
