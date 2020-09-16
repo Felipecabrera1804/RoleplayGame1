ea21528bf17b764f77754ef5d15bff14a418d077
Felipe <felipe.cabrera@correo.ucu.edu.uy>
2020-09-16 15:24:26 -0300

    parte 3

--- src/Library/Esapadachin.cs
+++ src/Library/Esapadachin.cs
@@ -9,14 +9,14 @@ namespace Library
         {
             this.Nombre = nombre;
             this.Vida = vida;
-            this.Ataque = ataque + espada.Daño;
             this.Defensa = defensa + escudo.Defensa;
             this.Resistencia = resistencia;
+            this.Attack=ataque + espada.Daño;
         }
 
         public int Defensa {get;set;}
         public int Resistencia {get;set;}
-        public int Ataque {get;set;}
+        public int Attack {get;set;}
         public string Nombre {get;set;}
         public int Vida {get;set;}
 
