fd504f66a9c08cd631a32146819b1a9891fb6344
luispee <51213393+luispee@users.noreply.github.com>
2020-09-17 23:35:08 -0300

    test bestia

--- src/Library/Bestia.cs
+++ src/Library/Bestia.cs
@@ -4,12 +4,16 @@ namespace Library
 {
     public class Bestia
     {
-        public Bestia(int fuerza, int defensa, int resistencia, Espada espada, Armadura armadura)
+        public Bestia(String nombre, int fuerza, int vida, int defensa, int resistencia, Mazo mazo, Chaleco chaleco)
         {
-            this.Ataque = fuerza + espada.Daño;
-            this.Defensa = defensa + armadura.Defensa;
-            this.Resistencia = resistencia + armadura.Resistencia;
+            this.Nombre=nombre;
+            this.Vida=vida;
+            this.Ataque = fuerza + mazo.Daño;
+            this.Defensa = defensa + chaleco.Defensa;
+            this.Resistencia = resistencia + chaleco.Resistencia;
         }
+        public string Nombre {get;set;}
+        public int Vida {get;set;}
         public int Defensa {get;set;}
         public int Resistencia {get;set;}
         public int Ataque {get;set;}
