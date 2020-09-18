fa2d5fa1062ac861756bfdd424e1fb0d62e8f98e
vbonelli91 <vicbonelli@gmail.com>
2020-09-18 12:37:30 -0300

    Update TestMago.cs

--- src/Test/Library.Test/TestMago.cs
+++ src/Test/Library.Test/TestMago.cs
@@ -5,7 +5,6 @@ using Library;
 namespace Test.Library
 {
 
-
     public class TestMago
     {
 
@@ -14,24 +13,30 @@ namespace Test.Library
         private Espada excalibur;
         private  Mago gandalf;
         private Armadura hierro;
-
         private Lanza gugnir; 
-
         private Enano gimli;
+        private Elfo elfi;
+        private LibroHechizos fimbulvetr;
 
+ [SetUp]
+        public void SetUp(){
+              this.vara= new Baston (50,90,95);
+              this.gucci= new Tunica (0,20,10);
+              this.excalibur= new Espada(40);
+              this.gandalf =new Mago("Gandalf", 10 , 20, 40, 60, 25, vara, gucci);
+              this.hierro= new Armadura(5,3,20);
+              this.gugnir= new Lanza(30);
+              this.gimli = new Enano("Gimli",100,25,20,5,excalibur,hierro);
+              this.fimbulvetr= new LibroHechizos("Fimbulvetr",15,60);
+              this.elfi= new Elfo(20,"Elfi",25,50,40,60,gugnir,hierro,fimbulvetr);
+
+        }
        [Test]
 
 //validacion al crear objeto, correctos atributo para ataque del mago 
 
         public void ValidValueAttack ()
         {
-        Baston vara = new Baston (50,90,95);
-        Tunica gucci = new Tunica (0,20,10);
-        Espada excalibur = new Espada(40);
-        Armadura hierro = new Armadura(5,3,20);
-        Lanza gugnir = new Lanza(30);
-        Mago gandalf = new Mago("Gandalf", 10 , 20, 40, 60, 25, vara, gucci);
-        Enano gimli = new Enano("Gimli",100,25,20,5,excalibur,hierro);
 
         Assert.AreEqual(90,gandalf.Ataque);
         }
@@ -42,29 +47,13 @@ namespace Test.Library
 
         public void ValidValueResistance ()
         {
-        Baston vara = new Baston (50,90,95);
-        Tunica gucci = new Tunica (0,20,10);
-        Espada excalibur = new Espada(40);
-        Armadura hierro = new Armadura(5,3,20);
-        Lanza gugnir = new Lanza(30);
-        Mago gandalf = new Mago("Gandalf", 10 , 20, 40, 60, 25, vara, gucci);
-        Enano gimli = new Enano("Gimli",100,25,20,5,excalibur,hierro);
-
-        Assert.AreEqual(45,gandalf.Resistencia);
+            Assert.AreEqual(45,gandalf.Resistencia);
         }
 
 
 [Test]
          public void ValidValueDefense ()
         {
-        Baston vara = new Baston (50,90,95);
-        Tunica gucci = new Tunica (0,20,10);
-        Espada excalibur = new Espada(40);
-        Armadura hierro = new Armadura(5,3,20);
-        Lanza gugnir = new Lanza(30);
-        Mago gandalf = new Mago("Gandalf", 10 , 20, 40, 60, 25, vara, gucci);
-        Enano gimli = new Enano("Gimli",100,25,20,5,excalibur,hierro);
-     
         Assert.AreEqual(60,gandalf.Defensa);
         }
         
@@ -75,21 +64,19 @@ namespace Test.Library
 [Test]
         public void valorAtaque()
     {
-         Baston vara = new Baston (50,90,95);
-        Tunica gucci = new Tunica (0,20,10);
-        Espada excalibur = new Espada(40);
-        Armadura hierro = new Armadura(5,3,20);
-        Lanza gugnir = new Lanza(30);
-        Mago gandalf = new Mago("Gandalf", 10 , 20, 40, 60, 25, vara, gucci);
-        Enano gimli = new Enano("Gimli",100,25,20,5,excalibur,hierro);
-
-       
-         Assert.AreEqual(5,Ataque.Atacar(gimli.Ataque, gandalf.Vida, gandalf.Defensa));
-    
-
+        Assert.AreEqual(5,Ataque.Atacar(gimli.Ataque, gandalf.Vida, gandalf.Defensa));
+   
     }
 
 
+//validacion curacion mago, verificar vida nueva Mago
+
+[Test]
+        public void valorCuracion()
+    {
+        Assert.AreEqual(30,Curar.Curacion(elfi.Cura,gandalf.Vida));
+   
+    }
 
 
 
